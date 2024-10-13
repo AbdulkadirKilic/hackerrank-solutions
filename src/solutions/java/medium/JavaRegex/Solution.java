cf141ffd258957d88dacd3534651b01e9a690718
AbdulkadirKilic <kilicabdulkadir32@gmail.com>
2024-10-13 13:49:58 +0300

    Add solution for HackerRank problem: Java Regex

---
+++ src/solutions/java/medium/JavaRegex/Solution.java
@@ -0,0 +1,22 @@
+// HackerRank Question: https://www.hackerrank.com/challenges/java-regex/problem
+
+package solutions.java.medium.JavaRegex;
+
+import java.util.Scanner;
+
+class Solution {
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    while (in.hasNext()) {
+      String IP = in.next();
+      System.out.println(IP.matches(new MyRegex().pattern));
+    }
+  }
+}
+
+class MyRegex {
+  String pattern =
+      "^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|0[0-9]{2}|[0-9]{1,2})\\.){3}"
+          + "(25[0-5]|2[0-4][0-9]|1[0-9]{2}|0[0-9]{2}|[0-9]{1,2})$";
+}

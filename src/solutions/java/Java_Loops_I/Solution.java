773e9af891bde6cfecd2995e8e7b4eb4da5172ae
AbdulkadirKilic <kilicabdulkadir32@gmail.com>
2024-10-13 15:58:01 +0300

    Add solution for HackerRank problem: Java Loops I

---
+++ src/solutions/java/Java_Loops_I/Solution.java
@@ -0,0 +1,19 @@
+// HackerRank Question: https://www.hackerrank.com/challenges/java-loops-i/problem
+
+package solutions.java.Java_Loops_I;
+
+import java.io.*;
+
+public class Solution {
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(bufferedReader.readLine().trim());
+    bufferedReader.close();
+
+    for (int i = 1; i <= 10; i++) {
+      int result = N * i;
+      System.out.println(N + " x " + i + " = " + result);
+    }
+  }
+}

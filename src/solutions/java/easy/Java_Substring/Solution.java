5e62072784f9274d310b7b2f4961b835991c363e
AbdulkadirKilic <kilicabdulkadir32@gmail.com>
2024-10-13 15:23:25 +0300

    Added solution for HackerRank problem: Java Substring

---
+++ src/solutions/java/easy/Java_Substring/Solution.java
@@ -0,0 +1,17 @@
+// HackerRank Question: https://www.hackerrank.com/challenges/java-substring/problem
+
+package solutions.java.easy.Java_Substring;
+
+import java.util.*;
+
+public class Solution {
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    String S = in.next();
+    int start = in.nextInt();
+    int end = in.nextInt();
+
+    System.out.println(S.substring(start, end));
+  }
+}

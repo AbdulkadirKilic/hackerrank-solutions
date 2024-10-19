d69e690175404a9a8933e71de66d82eb1c42876e
AbdulkadirKilic <kilicabdulkadir32@gmail.com>
2024-10-19 14:05:56 +0300

    Add solution for HackerRank problem: Java 1D Array (Part 2)

---
+++ src/solutions/java/Java_1D_Array_Part_2/Solution.java
@@ -0,0 +1,43 @@
+// HackerRank Question: https://www.hackerrank.com/challenges/java-1d-array/problem
+
+package solutions.java.Java_1D_Array_Part_2;
+
+import java.util.*;
+
+public class Solution {
+
+  public static boolean canWin(int leap, int[] game) {
+    return canWin(leap, game, 0);
+  }
+
+  private static boolean canWin(int leap, int[] game, int i) {
+
+    if (i >= game.length) {
+      return true;
+    }
+    if (i < 0 || game[i] == 1) {
+      return false;
+    }
+
+    game[i] = 1;
+
+    return canWin(leap, game, i + leap) || canWin(leap, game, i + 1) || canWin(leap, game, i - 1);
+  }
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int q = scan.nextInt();
+    while (q-- > 0) {
+      int n = scan.nextInt();
+      int leap = scan.nextInt();
+
+      int[] game = new int[n];
+      for (int i = 0; i < n; i++) {
+        game[i] = scan.nextInt();
+      }
+
+      System.out.println((canWin(leap, game)) ? "YES" : "NO");
+    }
+    scan.close();
+  }
+}

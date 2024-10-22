4bb3405403a9521eabdff8f69766a60cf11295d2
AbdulkadirKilic <kilicabdulkadir32@gmail.com>
2024-10-22 15:20:33 +0300

    Add solution for HackerRank problem: Covariant Return Types

---
+++ src/solutions/java/Covariant_Return_Types/Solution.java
@@ -0,0 +1,65 @@
+// HackerRank Question: https://www.hackerrank.com/challenges/java-covariance/problem
+
+package solutions.java.Covariant_Return_Types;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Flower {
+  String whatsYourName() {
+    return "I am just a Flower";
+  }
+}
+
+class Jasmine extends Flower {
+  @Override
+  String whatsYourName() {
+    return "Jasmine";
+  }
+}
+
+class Lily extends Flower {
+  @Override
+  String whatsYourName() {
+    return "Lily";
+  }
+}
+
+class Region {
+  Flower yourNationalFlower() {
+    return new Flower();
+  }
+}
+
+class WestBengal extends Region {
+  @Override
+  Jasmine yourNationalFlower() {
+    return new Jasmine();
+  }
+}
+
+class AndhraPradesh extends Region {
+  @Override
+  Lily yourNationalFlower() {
+    return new Lily();
+  }
+}
+
+public class Solution {
+  public static void main(String[] args) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    String s = reader.readLine().trim();
+    Region region = null;
+    switch (s) {
+      case "WestBengal":
+        region = new WestBengal();
+        break;
+      case "AndhraPradesh":
+        region = new AndhraPradesh();
+        break;
+    }
+    Flower flower = region.yourNationalFlower();
+    System.out.println(flower.whatsYourName());
+  }
+}

1cf74beff8a2fb230040fdacf1eada84c9d3fbde
AbdulkadirKilic <kilicabdulkadir32@gmail.com>
2024-10-24 13:42:55 +0300

    Added solution for HackerRank problem: Simple Array Sum

---
+++ src/solutions/data_structures/Simple_Array_Sum/Solution.java
@@ -0,0 +1,43 @@
+// HackerRank Question: https://www.hackerrank.com/challenges/simple-array-sum/problem
+
+package solutions.data_structures.Simple_Array_Sum;
+
+import static java.util.stream.Collectors.toList;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Result {
+
+  public static int simpleArraySum(List<Integer> ar) {
+    int sum = 0;
+    for (int num : ar) {
+      sum += num;
+    }
+    return sum;
+  }
+}
+
+public class Solution {
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bufferedWriter =
+        new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+    int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+    List<Integer> ar =
+        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+    int result = Result.simpleArraySum(ar);
+
+    bufferedWriter.write(String.valueOf(result));
+    bufferedWriter.newLine();
+
+    bufferedReader.close();
+    bufferedWriter.close();
+  }
+}

3d4147c6dee01fd45733ce706792b62da488c951
AbdulkadirKilic <kilicabdulkadir32@gmail.com>
2024-11-26 19:23:33 +0300

    Added solution for HackerRank problem: Matching {x, y} Repetitions

---
+++ src/solutions/regex/Matching_x_y_Repetitions/Solution.java
@@ -0,0 +1,27 @@
+// HackerRank Question: https://www.hackerrank.com/challenges/matching-x-y-repetitions/problem
+
+package solutions.regex.Matching_x_y_Repetitions;
+
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+  public static void main(String[] args) {
+
+    Regex_Test tester = new Regex_Test();
+    tester.checker("^[0-9]{1,2}[a-zA-Z]{3,}[!.,]{0,3}$");
+  }
+}
+
+class Regex_Test {
+
+  public void checker(String Regex_Pattern) {
+
+    Scanner Input = new Scanner(System.in);
+    String Test_String = Input.nextLine();
+    Pattern p = Pattern.compile(Regex_Pattern);
+    Matcher m = p.matcher(Test_String);
+    System.out.println(m.find());
+  }
+}

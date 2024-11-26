a9b99e5050deab009d5c53f947c8cfa534d9fac6
AbdulkadirKilic <kilicabdulkadir32@gmail.com>
2024-11-26 18:11:37 +0300

    Added solution for HackerRank problem: Matching Specific Characters

---
+++ src/solutions/regex/Matching_Specific_Characters/Solution.java
@@ -0,0 +1,27 @@
+// HackerRank Question: https://www.hackerrank.com/challenges/matching-specific-characters/problem
+
+package solutions.regex.Matching_Specific_Characters;
+
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+  public static void main(String[] args) {
+
+    Regex_Test tester = new Regex_Test();
+    tester.checker("^[123][120][xs0][3oOaA0][xsu][.,]$");
+  }
+}
+
+class Regex_Test {
+
+  public void checker(String Regex_Pattern) {
+
+    Scanner Input = new Scanner(System.in);
+    String Test_String = Input.nextLine();
+    Pattern p = Pattern.compile(Regex_Pattern);
+    Matcher m = p.matcher(Test_String);
+    System.out.println(m.find());
+  }
+}

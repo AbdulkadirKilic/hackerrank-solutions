ee07185642afa59362728656d6c80f12bf9f47e8
AbdulkadirKilic <kilicabdulkadir32@gmail.com>
2024-11-28 12:42:23 +0300

    Added solution for HackerRank problem: Negative Lookahead

---
+++ src/solutions/regex/Negative_Lookahead/Solution.java
@@ -0,0 +1,31 @@
+// HackerRank Question: https://www.hackerrank.com/challenges/negative-lookahead/problem
+
+package solutions.regex.Negative_Lookahead;
+
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+  public static void main(String[] args) {
+
+    Regex_Test tester = new Regex_Test();
+    tester.checker("(.)(?!\\1)");
+  }
+}
+
+class Regex_Test {
+
+  public void checker(String Regex_Pattern) {
+
+    Scanner Input = new Scanner(System.in);
+    String Test_String = Input.nextLine();
+    Pattern p = Pattern.compile(Regex_Pattern);
+    Matcher m = p.matcher(Test_String);
+    int Count = 0;
+    while (m.find()) {
+      Count += 1;
+    }
+    System.out.format("Number of matches : %d", Count);
+  }
+}

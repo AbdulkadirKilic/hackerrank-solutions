a6b3c41805e1ac7831026de9f2e05d25212347bc
AbdulkadirKilic <kilicabdulkadir32@gmail.com>
2024-12-07 10:18:11 +0300

    Added solution for HackerRank SQL problem: Revising Aggregations - The Count Function (Oracle SQL)

---
+++ src/solutions/sql/Revising_Aggregations_The_Count_Function.sql
@@ -0,0 +1,7 @@
+-- HackerRank Question: https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem
+
+SELECT COUNT(*) AS CityCount
+FROM CITY
+WHERE Population > 100000;
+
+

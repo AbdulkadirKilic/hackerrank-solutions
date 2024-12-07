c669fea1e46577d0642f94fdcfcd404c9a905861
AbdulkadirKilic <kilicabdulkadir32@gmail.com>
2024-12-07 10:22:33 +0300

    Added solution for HackerRank SQL problem: Revising Aggregations - Averages (Oracle SQL)

---
+++ src/solutions/sql/Revising_Aggregations_Averages.sql
@@ -0,0 +1,6 @@
+-- HackerRank Question: https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem
+
+SELECT AVG(Population) AS AveragePopulation
+FROM CITY
+WHERE District = 'California';
+

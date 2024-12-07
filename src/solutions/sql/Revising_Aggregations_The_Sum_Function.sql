49aa9be56cdd8ae6af3d288243c453e0b0bd6849
AbdulkadirKilic <kilicabdulkadir32@gmail.com>
2024-12-07 10:20:36 +0300

    Added solution for HackerRank SQL problem: Revising Aggregations - The Sum Function (Oracle SQL)

---
+++ src/solutions/sql/Revising_Aggregations_The_Sum_Function.sql
@@ -0,0 +1,7 @@
+-- HackerRank Question: https://www.hackerrank.com/challenges/revising-aggregations-sum/problem
+
+SELECT SUM(Population) AS TotalPopulation
+FROM CITY
+WHERE District = 'California';
+
+

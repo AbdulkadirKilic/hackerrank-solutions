d0d4d27fd493979d309e62c60dde71f1ccd2c104
AbdulkadirKilic <kilicabdulkadir32@gmail.com>
2024-12-08 13:09:54 +0300

    Added solution for HackerRank SQL problem: Weather Observation Station 13 (Oracle SQL)

---
+++ src/solutions/sql/Weather_Observation_Station_13.sql
@@ -0,0 +1,8 @@
+-- HackerRank Question: https://www.hackerrank.com/challenges/weather-observation-station-13/problem
+
+SELECT
+    TRUNC(SUM(LAT_N), 4) AS TotalLatN
+FROM STATION
+WHERE LAT_N > 38.7880 AND LAT_N < 137.2345;
+
+

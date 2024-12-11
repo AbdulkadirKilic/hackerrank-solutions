692ed80132d8d1d0fb58261ecbd3d03544729f1c
AbdulkadirKilic <kilicabdulkadir32@gmail.com>
2024-12-11 14:12:14 +0300

    Added solution for HackerRank SQL problem: Average Population of Each Continent (Oracle SQL)

---
+++ src/solutions/sql/Average_Population_of_Each_Continent.sql
@@ -0,0 +1,13 @@
+-- HackerRank Question: https://www.hackerrank.com/challenges/average-population-of-each-continent/problem
+
+SELECT
+    COUNTRY.Continent,
+    FLOOR(AVG(CITY.Population)) AS AveragePopulation
+FROM
+    CITY
+JOIN
+    COUNTRY
+ON
+    CITY.CountryCode = COUNTRY.Code
+GROUP BY
+    COUNTRY.Continent;
